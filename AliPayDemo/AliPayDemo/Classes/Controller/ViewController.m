061ffbb252116c7fdf41fc6229c9d185cc8ea6aa
zhangli <313890735@qq.com>
2016-02-21 16:03:13 +0800

    导入阿里的静态库

--- AliPayDemo/AliPayDemo/Classes/Controller/ViewController.m
+++ AliPayDemo/AliPayDemo/Classes/Controller/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "Product.h"
+#import "Order.h"
 
 @interface ViewController ()
 @property(nonatomic,strong)NSArray * products ;
@@ -65,11 +66,62 @@
 
 - (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-
+    // 1.取出模型对象
+    Product * product = self.products[indexPath.row];
+    //2.购买商品
+    [self buyProduct:product];
 }
 
+- (void) buyProduct:(Product *)product
+{
+   //1.添加partner/seller/privateKey，签约后获得
+    NSString * partner = @"" ;
+    NSString * seller = @"";
+    NSString * privateKey = @"" ;
+    
+    //2.生成订单
+        //2.1 创建订单
+    Order * order = [[Order alloc] init];
+        //2.2 设置商户ID/账号ID
+    order.partner = partner;
+    order.seller = seller ;
+        //2.3 设置订单号
+    order.tradeNO = nil ;
+        //2.4 设置产品相关的信息
+    order.productName = product.title ;
+    order.productDescription = product.detail ;
+    order.amount = [NSString stringWithFormat:@"%.2f",product.price];
+        //2.5 设置订单常量
+    order.service = @"moblie.securitypay.pay";
+    order.paymentType = @"1";
+    order.inputCharset = @"utf-8";
+    order.itBPay = @"30m";
+    order.showUrl = @"m.alipay.com";
+        //2.6 回调URL（异步通知服务器的地址）
+    order.notifyURL = @"http://www.baidu.com";
+        //2.7 将订单信息拼接成字符串
+    NSString * orderSpec = [order description];
+    
+    //3.对订单进行签名加密
+    
+    
+    
+}
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
